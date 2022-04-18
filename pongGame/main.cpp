64647363b22fb4065826268d3a7806f98d6850e4
constantinos <connoskats@gmail.com>
2022-04-18 14:14:32 +0100

    Fix for collision class

--- pongGame/main.cpp
+++ pongGame/main.cpp
@@ -33,7 +33,7 @@ public:
     maxY(frameheight),
     minY(0),
     rectangule(sf::Vector2f(width,height)),
-    vy(0.2)
+    vy(10)
     {           
         // Set origin to the middle of the rectangle instead of its corner
         rectangule.setOrigin(sf::Vector2f(width/2, height/2));
@@ -110,10 +110,10 @@ public:
     // Constructor and initialize the ball objects
     ball(int r, sf::Color color ):
     circle(r),
-    x(100),
-    y(100),
-    vx(0.5),
-    vy(0.5)
+    x(250),
+    y(450),
+    vx(-1),
+    vy(-1)
     {
         // Saving the ball radius used during construction
         radius = r;
@@ -133,55 +133,54 @@ public:
     }
 
     // Collision method changing velocity sign
-    void  updateSpeed(int frameWidth, int frameHeight){
+    void  updateSpeed(int frameWidth, int frameHeight)
+    {
         // If x position is outside the frame, change its sign
         if ( x < 0 || x > frameWidth )
         {
-            vx = vx * -1 ;
+            vx = -vx;
         }
 
         // If y position is outside the frame, change its sign
-        if ( y < 0 || y > frameHeight )
+        if ( y < 0 || y > frameHeight  )
         {
-            vy = vy * -1 ;
+            vy = -vy;
         }
-    }  
-    //Method to detect collision
-    void collision(paddle& paddleObject, 
-                   int frameWidth, 
-                   float factor,
-                   int frameHeight )
-    {   float ballAngle=75.f;
-        float ballSpeed=400.f;
-        vx = std::cos(ballAngle)*factor;
-        vy = std::sin(ballAngle)*factor;
+    } 
 
-        sf::RectangleShape topBorder;
-        topBorder.setSize(sf::Vector2f(frameWidth,paddleObject.x));
-        topBorder.setPosition(0,0);
-        sf::RectangleShape botBorder;
-        botBorder.setSize(sf::Vector2f(frameWidth,paddleObject.x));
-        botBorder.setPosition(0,frameHeight-paddleObject.x);
+    //Method to detect collision with paddle
+    //stored in array-vector of paddles
+    void collision(std::vector<paddle> &paddles, int frameWidth, int frameHeight)
+    //void collision(std::vector<paddle> paddles, int frameWidth, int frameHeight)
 
+    {     
+        for (int i = 0; i <=paddles.size(); i++)
+        
+        {     
+        //This is essentially a collision method that should be within the ball class
+            //std::cout <<paddles[i].x<< std::endl;      
+            //std::cout <<paddles[i].rectangule.getGlobalBounds().top<< std::endl;            
+            if ((circle.getGlobalBounds().intersects(paddles[i].rectangule.getGlobalBounds())))           
+            {
+                vx = -vx;
+            }
+        }
 
-        // DAA: This is essentially a collision method that should be within the ball class
-        if ((circle.getGlobalBounds().intersects(paddleObject.rectangule.getGlobalBounds())) || (circle.getGlobalBounds().intersects(paddleObject.rectangule.getGlobalBounds())))           
+        //if ((circle.getGlobalBounds().intersects(paddles[0].rectangule.getGlobalBounds())) )           
+        //{
+        //    vx = -vx;
+        //              
+        //}
+        //if ((circle.getGlobalBounds().intersects(paddles[1].rectangule.getGlobalBounds())) )           
+        //{
+        //    vx = -vx;
+        //              
+        //}
+
+        if ( y < 0 || y >frameHeight)
         {
-            circle.move(-vx,-vy);
-            vx = -(vy);
-            ballSpeed=sqrt(pow(vy,2)+pow(vy,2));
-            ballAngle = -ballAngle;
-            ballSpeed = -ballSpeed;               
+            vy = -vy;
         }
-
-        // DAA: This is essentially a collision method that should be within the ball class
-        if ((circle.getGlobalBounds().intersects(topBorder.getGlobalBounds()))||(circle.getGlobalBounds().intersects(botBorder.getGlobalBounds())))
-        {   
-            ballSpeed=sqrt(pow(vy,2)+pow(vx,2));
-            vx = -(vx);
-            ballAngle = -ballAngle;
-        }
-
     }
 
 };
@@ -196,6 +195,7 @@ int main()
     // Setting players scores
     int player1score = 0;
     int player2score = 0;
+    int tag  = 0;
 
     // Loading font file and alert in case the font is not found
     sf::Font font;
@@ -223,6 +223,12 @@ int main()
     paddle paddleLeft(frameWidth,frameHeight,0);
     paddle paddleRight(frameWidth,frameHeight,1);
 
+    //define an array-vector container class to store all object-paddles
+    std::vector<paddle> paddles;
+
+    //add paddles to array
+    paddles.push_back(paddleRight);
+    paddles.push_back(paddleLeft);
 
     // NOTE: These methods should go within the ball class! 
     //sf::RectangleShape topBorder;
@@ -239,11 +245,11 @@ int main()
     // NOTE: VideoMode is a class and myVideosize is the object!
     sf::VideoMode myVideosize(frameWidth, frameHeight);
     sf::RenderWindow window(myVideosize,"PongGame!");
-
+    window.setFramerateLimit(200);
     // NOTE: These methods should go within the ball class! 
-    sf::Vector2f velocity;
-    float ballSpeed = 400.f;
-    float ballAngle=75.f;
+    //sf::Vector2f velocity;
+    //float ballSpeed = 400.f;
+    //float ballAngle=75.f;
 
     // Boolean variables to check if keys are pressed
     bool isWPressed     = false;
@@ -259,8 +265,8 @@ int main()
         // I think the idea is to have velocity changes based on actual time rather than frames.
         // This is exactly what we needed.
         // Still, we need to move these methods to their corresponding classes.
-        float deltaTime = clock.restart().asSeconds();
-        float factor = deltaTime * ballSpeed;
+        //float deltaTime = clock.restart().asSeconds();
+        //float factor = deltaTime * ballSpeed;
 
 
         // DAA: We need to create a player class and have all these methods inside the player objects.
@@ -302,13 +308,13 @@ int main()
                     {
                         isGamePause = !isGamePause;
                         // DAA: This can also be included within the ball class
-                        ballSpeed = 0.0f;                      
+                        //ballSpeed = 0.0f;                      
                     } 
                     if(event.key.code == sf::Keyboard::N)  
                     {
                         isGamePause = false;
                         // DAA: This can also be included within the ball class
-                        ballSpeed = 400.0f;
+                        //ballSpeed = 400.0f;
                     } 
                 break;
                 
@@ -356,8 +362,8 @@ int main()
         window.draw(ball1.circle);
 
         // Drawing the paddles
-        window.draw(paddleLeft.rectangule);
-        window.draw(paddleRight.rectangule);
+        window.draw(paddles[0].rectangule);
+        window.draw(paddles[1].rectangule);
 
            
         // Option 2 to move the ball 
@@ -379,10 +385,13 @@ int main()
         //    ballAngle = -ballAngle;
         //}
 
-        //ball1.updateSpeed(frameWidth, frameHeight);
-        ball1.collision(paddleRight, frameWidth, factor, frameHeight);
-        ball1.collision(paddleLeft, frameWidth, factor, frameHeight);
+        
+        ball1.updatePos();
 
+
+        //ball1.collision(paddles, frameWidth, frameHeight);
+        ball1.collision(paddles, frameWidth, frameHeight);
+          
         // When scoring
         if (ball1.circle.getPosition().x > paddleRight.rectangule.getPosition().x + 2*ball1.radius)
         {
@@ -390,10 +399,10 @@ int main()
             player1score++;
             
             // DAA: We can create a reset method within the ball class to reset its position
-            ball1.circle.setPosition(frameHeight/2,frameWidth/2);
+            //ball1.circle.setPosition(frameHeight/2,frameWidth/2);
 
             // DAA: We have to add a beautiful scoreboard within the game to display the score
-            std::cout <<"Player 1 scored:"<<player1score << std::endl;
+            //std::cout <<"Player 1 scored:"<<player1score << std::endl;
         }
         else if (ball1.circle.getPosition().x < paddleLeft.rectangule.getPosition().x - 2*ball1.radius)
         {
@@ -401,10 +410,10 @@ int main()
             player2score++;
  
             // DAA: We can create a reset method within the ball class to reset its position
-            ball1.circle.setPosition(frameHeight/2,frameWidth/2);
+            //ball1.circle.setPosition(frameHeight/2,frameWidth/2);
 
             // DAA: We have to add a beautiful scoreboard within the game to display the score
-            std::cout <<"Player 2 scored:"<<player1score << std::endl;
+            //std::cout <<"Player 2 scored:"<<player1score << std::endl;
         }
 
         // If pause, write pause on screen.
@@ -413,10 +422,10 @@ int main()
         //       is exactly the same as
         //       if ( booleanVariable ) {}
         //       The  "== True" is not needed
-        if ( isGamePause ){
-            pause.setPosition(300,200);
-            window.draw(pause);
-        }
+        //if ( isGamePause ){
+        //    pause.setPosition(300,200);
+        //    window.draw(pause);
+        //}
 
         // Display window    
         window.display();
